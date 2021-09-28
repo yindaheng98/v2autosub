e7af02dc8ef028dfc079f8407e80402840ae1e1f
yindaheng98 <yindaheng98@163.com>
2021-09-28 19:07:18 +0800

    Balancer finish

--- ConfGen.go
+++ ConfGen.go
@@ -4,8 +4,30 @@ import (
 	"io/ioutil"
 	"iochen.com/v2gen/v2"
 	"iochen.com/v2gen/v2/infra"
-	"strings"
 )
+
+func BasicConf(tmplPath, confPath string, defaultTmpl []byte, exConf map[string]string) string {
+	tmpl := defaultTmpl
+	if tmplPath != "" {
+		t, err := ioutil.ReadFile(tmplPath)
+		if err == nil {
+			tmpl = t
+		}
+	}
+	v2genConf := infra.V2genConfig{}
+	confFile, err := ioutil.ReadFile(confPath)
+	if err == nil {
+		v2genConf = infra.ParseV2genConf(confFile)
+	}
+	conf := infra.DefaultConf()
+	bytes, err := infra.GenV2RayConf(*conf.Append(v2genConf).Append(exConf), tmpl)
+	if err != nil {
+		panic(err)
+	}
+	v2rayConf := string(bytes)
+	return v2rayConf
+
+}
 
 type LinkConf struct {
 	name     string
@@ -15,24 +37,29 @@ type LinkConf struct {
 }
 
 func (lc *LinkConf) String() string {
-	tmpl := []byte(DefaultLinkConfTmpl)
-	if lc.tmplPath != "" {
-		t, err := ioutil.ReadFile(lc.tmplPath)
-		if err == nil {
-			tmpl = t
-		}
+	replace := lc.link.Config()
+	replace["name"] = lc.name
+	v2rayConf := BasicConf(lc.tmplPath, lc.confPath, []byte(DefaultLinkConfTmpl), replace)
+	return v2rayConf
+}
+
+type LinksConf struct {
+	linkConfs []LinkConf
+	tmplPath  string
+	confPath  string
+}
+
+func (lsc *LinksConf) String() string {
+	tags := ""
+	outbounds := ""
+	for _, lc := range lsc.linkConfs {
+		tags += ",\"" + lc.name + "\""
+		outbounds += "," + lc.String()
 	}
-	v2genConf := infra.V2genConfig{}
-	confFile, err := ioutil.ReadFile(lc.confPath)
-	if err == nil {
-		v2genConf = infra.ParseV2genConf(confFile)
-	}
-	conf := infra.DefaultConf()
-	bytes, err := infra.GenV2RayConf(*conf.Append(v2genConf).Append(lc.link.Config()), tmpl)
-	if err != nil {
-		panic(err)
-	}
-	v2rayConf := string(bytes)
-	v2rayConf = strings.ReplaceAll(v2rayConf, "{{name}}", lc.name)
+	tags = tags[1:]
+	outbounds = outbounds[1:]
+
+	replace := map[string]string{"outbounds": outbounds, "tags": tags}
+	v2rayConf := BasicConf(lsc.tmplPath, lsc.confPath, []byte(DefaultLinksConfTmpl), replace)
 	return v2rayConf
 }
